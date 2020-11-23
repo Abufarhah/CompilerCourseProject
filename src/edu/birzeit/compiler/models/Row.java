9580b18af99ba2c16003dc04e5976a8ad1224b06
Abufarhah <laythabufarhah@gmail.com>
2020-11-23 13:22:27 +0200

    First Release

--- src/edu/birzeit/compiler/models/Row.java
+++ src/edu/birzeit/compiler/models/Row.java
@@ -1,11 +1,27 @@
 package edu.birzeit.compiler.models;
 
-public abstract class Row {
+public class Row {
     private String name;
     private String v1;
     private String v2;
     private String v3;
     private String v4;
+    private String v5;
+    private String v6;
+    private String v7;
+    private String v8;
+    private String v9;
+    private String v10;
+    private String v11;
+    private String v12;
+    private String v13;
+    private String v14;
+    private String v15;
+    private String v16;
+    private String v17;
+    private String v18;
+    private String v19;
+    private String v20;
 
     public Row() {
     }
@@ -14,13 +30,6 @@ public abstract class Row {
         this.name = name;
     }
 
-    public Row(String name, String v1, String v2, String v3, String v4) {
-        this.name = name;
-        this.v1 = v1;
-        this.v2 = v2;
-        this.v3 = v3;
-        this.v4 = v4;
-    }
 
     public String getName() {
         return name;
@@ -60,5 +69,133 @@ public abstract class Row {
 
     public void setV4(String v4) {
         this.v4 = v4;
+    }
+
+    public String getV5() {
+        return v5;
+    }
+
+    public void setV5(String v5) {
+        this.v5 = v5;
+    }
+
+    public String getV6() {
+        return v6;
+    }
+
+    public void setV6(String v6) {
+        this.v6 = v6;
+    }
+
+    public String getV7() {
+        return v7;
+    }
+
+    public void setV7(String v7) {
+        this.v7 = v7;
+    }
+
+    public String getV8() {
+        return v8;
+    }
+
+    public void setV8(String v8) {
+        this.v8 = v8;
+    }
+
+    public String getV9() {
+        return v9;
+    }
+
+    public void setV9(String v9) {
+        this.v9 = v9;
+    }
+
+    public String getV10() {
+        return v10;
+    }
+
+    public void setV10(String v10) {
+        this.v10 = v10;
+    }
+
+    public String getV11() {
+        return v11;
+    }
+
+    public void setV11(String v11) {
+        this.v11 = v11;
+    }
+
+    public String getV12() {
+        return v12;
+    }
+
+    public void setV12(String v12) {
+        this.v12 = v12;
+    }
+
+    public String getV13() {
+        return v13;
+    }
+
+    public void setV13(String v13) {
+        this.v13 = v13;
+    }
+
+    public String getV14() {
+        return v14;
+    }
+
+    public void setV14(String v14) {
+        this.v14 = v14;
+    }
+
+    public String getV15() {
+        return v15;
+    }
+
+    public void setV15(String v15) {
+        this.v15 = v15;
+    }
+
+    public String getV16() {
+        return v16;
+    }
+
+    public void setV16(String v16) {
+        this.v16 = v16;
+    }
+
+    public String getV17() {
+        return v17;
+    }
+
+    public void setV17(String v17) {
+        this.v17 = v17;
+    }
+
+    public String getV18() {
+        return v18;
+    }
+
+    public void setV18(String v18) {
+        this.v18 = v18;
+    }
+
+    public String getV19() {
+        return v19;
+    }
+
+    public void setV19(String v19) {
+        this.v19 = v19;
+    }
+
+    public String getV20() {
+        return v20;
+    }
+
+    public void setV20(String v20) {
+        this.v20 = v20;
     }
 }
--- src/edu/birzeit/compiler/models/RowFactory.java
+++ src/edu/birzeit/compiler/models/RowFactory.java
@@ -4,20 +4,244 @@ import java.util.List;
 
 public class RowFactory {
     public static Row getInstance(int attributes, String name, List<String> list) {
-        if(list.size()!=0) {
+        if (list.size() != 0) {
+            Row row = new Row(name);
             if (attributes == 1) {
-                return new RowOne(name, list.get(0));
+                row.setV1(list.get(0));
             } else if (attributes == 2) {
-                return new RowTwo(name, list.get(0), list.get(1));
+                row.setV1(list.get(0));
+                row.setV2(list.get(1));
             } else if (attributes == 3) {
-                return new RowThree(name, list.get(0), list.get(1), list.get(2));
+                row.setV1(list.get(0));
+                row.setV2(list.get(1));
+                row.setV3(list.get(2));
             } else if (attributes == 4) {
-                return new RowFour(name, list.get(0), list.get(1), list.get(2), list.get(3));
+                row.setV1(list.get(0));
+                row.setV2(list.get(1));
+                row.setV3(list.get(2));
+                row.setV4(list.get(3));
+            } else if (attributes == 5) {
+                row.setV1(list.get(0));
+                row.setV2(list.get(1));
+                row.setV3(list.get(2));
+                row.setV4(list.get(3));
+                row.setV5(list.get(4));
+            } else if (attributes == 6) {
+                row.setV1(list.get(0));
+                row.setV2(list.get(1));
+                row.setV3(list.get(2));
+                row.setV4(list.get(3));
+                row.setV5(list.get(4));
+                row.setV6(list.get(5));
+            } else if (attributes == 7) {
+                row.setV1(list.get(0));
+                row.setV2(list.get(1));
+                row.setV3(list.get(2));
+                row.setV4(list.get(3));
+                row.setV5(list.get(4));
+                row.setV6(list.get(5));
+                row.setV7(list.get(6));
+            } else if (attributes == 8) {
+                row.setV1(list.get(0));
+                row.setV2(list.get(1));
+                row.setV3(list.get(2));
+                row.setV4(list.get(3));
+                row.setV5(list.get(4));
+                row.setV6(list.get(5));
+                row.setV7(list.get(6));
+                row.setV8(list.get(7));
+            } else if (attributes == 9) {
+                row.setV1(list.get(0));
+                row.setV2(list.get(1));
+                row.setV3(list.get(2));
+                row.setV4(list.get(3));
+                row.setV5(list.get(4));
+                row.setV6(list.get(5));
+                row.setV7(list.get(6));
+                row.setV8(list.get(7));
+                row.setV9(list.get(8));
+            } else if (attributes == 10) {
+                row.setV1(list.get(0));
+                row.setV2(list.get(1));
+                row.setV3(list.get(2));
+                row.setV4(list.get(3));
+                row.setV5(list.get(4));
+                row.setV6(list.get(5));
+                row.setV7(list.get(6));
+                row.setV8(list.get(7));
+                row.setV9(list.get(8));
+                row.setV10(list.get(9));
+            } else if (attributes == 11) {
+                row.setV1(list.get(0));
+                row.setV2(list.get(1));
+                row.setV3(list.get(2));
+                row.setV4(list.get(3));
+                row.setV5(list.get(4));
+                row.setV6(list.get(5));
+                row.setV7(list.get(6));
+                row.setV8(list.get(7));
+                row.setV9(list.get(8));
+                row.setV10(list.get(9));
+                row.setV11(list.get(10));
+            } else if (attributes == 12) {
+                row.setV1(list.get(0));
+                row.setV2(list.get(1));
+                row.setV3(list.get(2));
+                row.setV4(list.get(3));
+                row.setV5(list.get(4));
+                row.setV6(list.get(5));
+                row.setV7(list.get(6));
+                row.setV8(list.get(7));
+                row.setV9(list.get(8));
+                row.setV10(list.get(9));
+                row.setV11(list.get(10));
+                row.setV12(list.get(11));
+            } else if (attributes == 13) {
+                row.setV1(list.get(0));
+                row.setV2(list.get(1));
+                row.setV3(list.get(2));
+                row.setV4(list.get(3));
+                row.setV5(list.get(4));
+                row.setV6(list.get(5));
+                row.setV7(list.get(6));
+                row.setV8(list.get(7));
+                row.setV9(list.get(8));
+                row.setV10(list.get(9));
+                row.setV11(list.get(10));
+                row.setV12(list.get(11));
+                row.setV13(list.get(12));
+            } else if (attributes == 14) {
+                row.setV1(list.get(0));
+                row.setV2(list.get(1));
+                row.setV3(list.get(2));
+                row.setV4(list.get(3));
+                row.setV5(list.get(4));
+                row.setV6(list.get(5));
+                row.setV7(list.get(6));
+                row.setV8(list.get(7));
+                row.setV9(list.get(8));
+                row.setV10(list.get(9));
+                row.setV11(list.get(10));
+                row.setV12(list.get(11));
+                row.setV13(list.get(12));
+                row.setV14(list.get(13));
+            } else if (attributes == 15) {
+                row.setV1(list.get(0));
+                row.setV2(list.get(1));
+                row.setV3(list.get(2));
+                row.setV4(list.get(3));
+                row.setV5(list.get(4));
+                row.setV6(list.get(5));
+                row.setV7(list.get(6));
+                row.setV8(list.get(7));
+                row.setV9(list.get(8));
+                row.setV10(list.get(9));
+                row.setV11(list.get(10));
+                row.setV12(list.get(11));
+                row.setV13(list.get(12));
+                row.setV14(list.get(13));
+                row.setV15(list.get(14));
+            } else if (attributes == 16) {
+                row.setV1(list.get(0));
+                row.setV2(list.get(1));
+                row.setV3(list.get(2));
+                row.setV4(list.get(3));
+                row.setV5(list.get(4));
+                row.setV6(list.get(5));
+                row.setV7(list.get(6));
+                row.setV8(list.get(7));
+                row.setV9(list.get(8));
+                row.setV10(list.get(9));
+                row.setV11(list.get(10));
+                row.setV12(list.get(11));
+                row.setV13(list.get(12));
+                row.setV14(list.get(13));
+                row.setV15(list.get(14));
+                row.setV16(list.get(15));
+            } else if (attributes == 17) {
+                row.setV1(list.get(0));
+                row.setV2(list.get(1));
+                row.setV3(list.get(2));
+                row.setV4(list.get(3));
+                row.setV5(list.get(4));
+                row.setV6(list.get(5));
+                row.setV7(list.get(6));
+                row.setV8(list.get(7));
+                row.setV9(list.get(8));
+                row.setV10(list.get(9));
+                row.setV11(list.get(10));
+                row.setV12(list.get(11));
+                row.setV13(list.get(12));
+                row.setV14(list.get(13));
+                row.setV15(list.get(14));
+                row.setV16(list.get(15));
+                row.setV17(list.get(16));
+            } else if (attributes == 18) {
+                row.setV1(list.get(0));
+                row.setV2(list.get(1));
+                row.setV3(list.get(2));
+                row.setV4(list.get(3));
+                row.setV5(list.get(4));
+                row.setV6(list.get(5));
+                row.setV7(list.get(6));
+                row.setV8(list.get(7));
+                row.setV9(list.get(8));
+                row.setV10(list.get(9));
+                row.setV11(list.get(10));
+                row.setV12(list.get(11));
+                row.setV13(list.get(12));
+                row.setV14(list.get(13));
+                row.setV15(list.get(14));
+                row.setV16(list.get(15));
+                row.setV17(list.get(16));
+                row.setV18(list.get(17));
+            } else if (attributes == 19) {
+                row.setV1(list.get(0));
+                row.setV2(list.get(1));
+                row.setV3(list.get(2));
+                row.setV4(list.get(3));
+                row.setV5(list.get(4));
+                row.setV6(list.get(5));
+                row.setV7(list.get(6));
+                row.setV8(list.get(7));
+                row.setV9(list.get(8));
+                row.setV10(list.get(9));
+                row.setV11(list.get(10));
+                row.setV12(list.get(11));
+                row.setV13(list.get(12));
+                row.setV14(list.get(13));
+                row.setV15(list.get(14));
+                row.setV16(list.get(15));
+                row.setV17(list.get(16));
+                row.setV18(list.get(17));
+                row.setV19(list.get(18));
+            } else if (attributes == 20) {
+                row.setV1(list.get(0));
+                row.setV2(list.get(1));
+                row.setV3(list.get(2));
+                row.setV4(list.get(3));
+                row.setV5(list.get(4));
+                row.setV6(list.get(5));
+                row.setV7(list.get(6));
+                row.setV8(list.get(7));
+                row.setV9(list.get(8));
+                row.setV10(list.get(9));
+                row.setV11(list.get(10));
+                row.setV12(list.get(11));
+                row.setV13(list.get(12));
+                row.setV14(list.get(13));
+                row.setV15(list.get(14));
+                row.setV16(list.get(15));
+                row.setV17(list.get(16));
+                row.setV18(list.get(17));
+                row.setV19(list.get(18));
+                row.setV20(list.get(19));
             } else {
                 return null;
             }
-        }else{
-            return new RowZero(name);
+            return row;
+        } else {
+            return new Row(name);
         }
     }
 }
